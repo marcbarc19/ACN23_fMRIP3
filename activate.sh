023924600c2bd38bd0430ad6d0d931f409547bce
marcbarc19 <65872535+marcbarc19@users.noreply.github.com>
2023-11-26 21:20:14 +0100

    Create activate.sh

---
+++ activate.sh
@@ -0,0 +1,3 @@
+#. /work/807746/marcs_folder/notebooks/virt_env/bin/activate  <--- Replace with own directory
+python -m ipykernel install --user --name=virt_env
+echo Done! Remember changing the kernel in Jupyter.
